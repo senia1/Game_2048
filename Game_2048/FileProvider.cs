eaefa2320b774e5398d013bf81c7b1f0d3d2df1b
senia <oks314@gmail.com>
2023-02-06 13:56:23 +0300

    added User and UserManager Classes

--- Game_2048/FileProvider.cs
+++ Game_2048/FileProvider.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Game_2048
 {
-    internal class FileProvider
+    public class FileProvider
     {
+        public static void Replace(string path, string value)
+        {
+            var writer = new StreamWriter(path, false, Encoding.UTF8);
+            writer.WriteLine(value);
+            writer.Close();
+        }
+
+        public static string Get(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public static bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
     }
 }
