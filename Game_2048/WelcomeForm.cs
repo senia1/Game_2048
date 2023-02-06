cf081e66e31274a2a2db67ce8a75b4aa29d00db0
senia <oks314@gmail.com>
2023-02-06 14:30:49 +0300

    added WelcomeForm

---
+++ Game_2048/WelcomeForm.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Game_2048
+{
+    public partial class WelcomeForm : Form
+    {
+        public List<System.Windows.Forms.RadioButton> radioButtons;
+        public WelcomeForm()
+        {
+            InitializeComponent();
+            radioButtons = new List<System.Windows.Forms.RadioButton>
+            {
+                radioButton1, radioButton2, radioButton3, radioButton4
+            };
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userNameTextBox.Text))
+            {
+                MessageBox.Show("Please, enter your name");
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+}
