d807d20a0bf364b1dcf6b0d15e6d74f1aa269b1f
senia <oks314@gmail.com>
2023-02-08 08:40:37 +0300

    developed several methods

--- Game_2048/MainForm.Designer.cs
+++ Game_2048/MainForm.Designer.cs
@@ -35,6 +35,10 @@
             this.rulesOfTheGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.resultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scoreLabelLabel = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.bestScoreLabelLabel = new System.Windows.Forms.Label();
+            this.bestScoreLabel = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +50,7 @@
             this.resultsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.Size = new System.Drawing.Size(620, 33);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -88,11 +92,51 @@
             this.resultsToolStripMenuItem.Text = "Results";
             this.resultsToolStripMenuItem.Click += new System.EventHandler(this.resultsToolStripMenuItem_Click);
             // 
+            // scoreLabelLabel
+            // 
+            this.scoreLabelLabel.AutoSize = true;
+            this.scoreLabelLabel.Location = new System.Drawing.Point(12, 47);
+            this.scoreLabelLabel.Name = "scoreLabelLabel";
+            this.scoreLabelLabel.Size = new System.Drawing.Size(60, 25);
+            this.scoreLabelLabel.TabIndex = 1;
+            this.scoreLabelLabel.Text = "Score:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(147, 47);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(22, 25);
+            this.scoreLabel.TabIndex = 2;
+            this.scoreLabel.Text = "0";
+            // 
+            // bestScoreLabelLabel
+            // 
+            this.bestScoreLabelLabel.AutoSize = true;
+            this.bestScoreLabelLabel.Location = new System.Drawing.Point(363, 47);
+            this.bestScoreLabelLabel.Name = "bestScoreLabelLabel";
+            this.bestScoreLabelLabel.Size = new System.Drawing.Size(83, 25);
+            this.bestScoreLabelLabel.TabIndex = 3;
+            this.bestScoreLabelLabel.Text = "The best:";
+            // 
+            // bestScoreLabel
+            // 
+            this.bestScoreLabel.AutoSize = true;
+            this.bestScoreLabel.Location = new System.Drawing.Point(529, 47);
+            this.bestScoreLabel.Name = "bestScoreLabel";
+            this.bestScoreLabel.Size = new System.Drawing.Size(22, 25);
+            this.bestScoreLabel.TabIndex = 4;
+            this.bestScoreLabel.Text = "0";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(620, 710);
+            this.Controls.Add(this.bestScoreLabel);
+            this.Controls.Add(this.bestScoreLabelLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.scoreLabelLabel);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -117,5 +161,9 @@
         private ToolStripMenuItem rulesOfTheGameToolStripMenuItem;
         private ToolStripMenuItem exitToolStripMenuItem;
         private ToolStripMenuItem resultsToolStripMenuItem;
+        private Label scoreLabelLabel;
+        private Label scoreLabel;
+        private Label bestScoreLabelLabel;
+        private Label bestScoreLabel;
     }
 }
--- Game_2048/MainForm.cs
+++ Game_2048/MainForm.cs
@@ -2,6 +2,8 @@ namespace Game_2048
 {
     public partial class MainForm : Form
     {
+        private const int mapSize = 4;
+        private Label[,] labelsMap; 
         public MainForm()
         {
             InitializeComponent();
@@ -9,7 +11,40 @@ namespace Game_2048
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            InitMap();
+        }
 
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize,mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLabel = CreateLabel(i,j);
+                    Controls.Add(newLabel);
+                    labelsMap[i,j] = newLabel;
+                }
+            }
+        }
+
+        private Label CreateLabel(int indexRow, int indexColumn)
+        {
+            var label = new Label();
+
+            label.BackColor = SystemColors.ButtonShadow;
+            label.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label.Size = new Size(140, 140);
+            //label.Text = number.ToString();
+            label.TextAlign = ContentAlignment.MiddleCenter;
+
+            int x = 12 + indexColumn * 152;
+            int y = 100 + indexRow * 152;
+
+            label.Location = new Point(x, y);
+
+            return label;
         }
 
         private void startToolStripMenuItem_Click(object sender, EventArgs e)
